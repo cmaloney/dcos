89a266a9b772d9421b7da354e2c91a10c33a6ff9
Cody Maloney <cody@mesosphere.io>
2016-11-02 16:05:12 -0700

    WIP

--- test_util/fetch_dcos_versions.py
+++ test_util/fetch_dcos_versions.py
@@ -2,27 +2,55 @@
 version of DC/OS for later use
 
 usage:
-fetch_dcos channel start_version end_version
-fetch_dcos exact <channel> <channel> <channel>
+fetch_dcos channel [start_version-end_version|version]...  [--download-packages]
+fetch_dcos exact <channel> <channel> <channel> [--download-packages]
   - Figures out the url to download from
   - fetches the dcos_generate_config.sh
   - Based on version in metadata.json, finds list of packages
   - Writes out the list of packages
   - Can fetch all the packages if asked.
-  - TODO(cmaloney): Should it write how to interact / interface for doing a genconf?
 
 Output layout
 <version>/
     0/
         metadata.json
         packages/
+            a-b.tar.gz
+            b-c.tar.gz
+            ...
         dcos_generate_config.sh
 """
 
 import requests
 
 
-def fetch_dgc_and_packages(channel):
+class Version:
+
+    @staticmethod
+    def from_str(version_str: str):
+        parts = version_str.split('-', 2)
+        if len(parts) == 1:
+            return Version(parts[0].split('.'), None)
+        raise NotImplementedError()
+
+    def __init__(self, components: list, tag):
+        assert isinstance(components, list)
+
+
+    def __lt__(self, other):
+        raise NotImplementedError()
+
+    def __gt__(self, other):
+        raise NotImplementedError()
+
+def get_base_url(version: Version):
+    if version < Version.from_str("1.7"):
+        return 'https://downloads.mesosphere.com/dcos'
+    return 'https://downloads.dcos.io/dcos'
+
+
+def fetch_dgc_and_packages(site, channel):
+    requests.get()
   raise NotImplementedError()
 
 """
