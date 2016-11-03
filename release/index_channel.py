f5dbcd90d84a7edcd99c184f12b223e09adfa2bb
Cody Maloney <cody@mesosphere.io>
2016-11-02 18:46:32 -0700

    WIP: Package downloader

---
+++ release/index_channel.py
@@ -0,0 +1,5 @@
+"""Update a version index inside a DC/OS release channel
+
+Scans the "commit" folder for releases
+Builds a dictionary from version -> commit channel path
+"""
--- test_util/fetch_dcos_versions.py
+++ test_util/fetch_dcos_versions.py
@@ -12,16 +12,20 @@ fetch_dcos exact <channel> <channel> <channel> [--download-packages]
 
 Output layout
 <version>/
-    0/
-        metadata.json
-        packages/
-            a-b.tar.gz
-            b-c.tar.gz
-            ...
-        dcos_generate_config.sh
+    metadata.json
+    packages/
+        a-b.tar.gz
+        b-c.tar.gz
+        ...
+    dcos_generate_config.sh
 """
 
-import requests
+import py
+
+
+from pkgpanda.util import download_chunked
+from release import from_json
+from release.storage.http import HttpStorageProvider
 
 
 class Version:
@@ -29,19 +33,33 @@ class Version:
     @staticmethod
     def from_str(version_str: str):
         parts = version_str.split('-', 2)
+        components = parts[0].split('.')
         if len(parts) == 1:
-            return Version(parts[0].split('.'), None)
-        raise NotImplementedError()
+            return Version(components, None)
+        else:
+            assert len(parts) == 2
+            return Version(components, parts[1])
 
     def __init__(self, components: list, tag):
-        assert isinstance(components, list)
-
+        self._components = components
+        self._tag = tag
 
     def __lt__(self, other):
+
+        raise NotImplementedError()
+
+    def __lte__(self, other):
         raise NotImplementedError()
 
     def __gt__(self, other):
         raise NotImplementedError()
+
+    def __gte__(self, other):
+        raise NotImplementedError()
+
+    def __str__(self):
+        return '{}{}'.format('.'.join(self._components), '-{}'.format(self._tag) if self._tag else '')
+
 
 def get_base_url(version: Version):
     if version < Version.from_str("1.7"):
@@ -49,31 +67,52 @@ def get_base_url(version: Version):
     return 'https://downloads.dcos.io/dcos'
 
 
-def fetch_dgc_and_packages(site, channel):
-    requests.get()
-  raise NotImplementedError()
+def version_from_metadata(metadata):
+    version = Version.from_str(metadata['tag'])
 
-"""
-Download stuff
- - Make list of all packages
- - Be able to download all packages
+    # DCOS < 1.7 used a "Canis Major" (CM) codename which is what is in the tag
+    if version.startswith('CM'):
+        version = Version(['1'] + version.components[1:], version.tag)
 
-if < 1.7
-downloads.mesosphere.com/dcos
-if > 1.7
-downloads.mesosphere.com/dcos-enterprise
-or
-  downloads.dcos.io/dcos
+    return version
 
 
-Need a meta-indexer + on release promote update index
+def fetch_dgc_and_packages(base_url, channel, download_packages: bool):
+    base_url = base_url.rtrim('/')
+    channel = channel.trim('/')
 
-don't reveal enterprise urls / have enterprise
+    store = HttpStorageProvider('{}/{}'.format(base_url, channel))
 
+    metadata_contents = store.fetch('metadata.json')
+    metadata = from_json(metadata_contents)
+    version = version_from_metadata(metadata)
 
+    """
+    <version>/
+        metadata.json
+        packages/
+            a-b.tar.gz
+            b-c.tar.gz
+            ...
+        dcos_generate_config.sh
+    """
+    path = py.path.local(str(version))
+    assert not path.exists(), "Only one instance of a given version can be grabbed currently."
+    work_path = py.path.local(str(version) + '.tmp')
 
-Need to make sure to have the package lists
-need to have an index of all releases in a channel
+    work_path.join('metadata.json').write(metadata_contents, ensure=True)
+    # TODO(cmaloney): Support variants
+    store.download('dcos_generate_config.sh', str(work_path.join('dcos_generate_config.sh')))
 
+    if not download_packages:
+        return
 
-"""
+    package_dir = work_path.join('packages')
+    package_dir.ensure()
+
+    repository_url = metadata['repository_url']
+
+    # TODO(cmaloney): This logic will change quite a bit based on DC/OS version
+    for package in metadata['all_completes'][None]:
+        package_filename = '{}.tar.xz'.format(package)
+        download_chunked(package_filename, '{}/packages/{}'.format(repository_url, package_filename))
